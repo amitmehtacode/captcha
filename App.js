97d7298c244b74aa2c49d73fba6d745270f950b0
Amit Kumar <amit.kumar8@tothenew.com>
2024-04-29 17:34:17 +0530

    specific to v2 changes

--- App.js
+++ App.js
@@ -2,14 +2,12 @@ import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import CaptchaV2Lib1 from './src/components/CaptchaV2Lib1';
 import CaptchaV2Lib2 from './src/components/CaptchaV2Lib2';
-import CaptchaV3Lib1 from './src/components/CaptchaV3Lib1';
-
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.heading}>reCAPTCHA V2</Text>
-      <CaptchaV2Lib2 />
+      <CaptchaV2Lib1 />
     </SafeAreaView>
   );
 };
